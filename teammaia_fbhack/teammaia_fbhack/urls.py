96a1cca0ba5a8f5f833fa19f46b56b492e7e8e21
gabrielhpugliese <gabrielhpugliese@gmail.com>
2012-05-18 18:29:37 -0300

    Projeto criado

---
+++ teammaia_fbhack/teammaia_fbhack/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^login/', 'django_fukinbook.views.login', name='login'),
+    url(r'^canvas/', 'django_fukinbook.views.canvas', name='canvas'),
+    url(r'^admin/', include(admin.site.urls)),
+)
